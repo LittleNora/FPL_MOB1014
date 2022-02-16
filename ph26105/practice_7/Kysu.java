0dccd8a274359cae45715adc346c786fb49b1da7
Nguyễn Quang Vinh <vinhndqph26105@fpt.edu.vn>
2022-02-16 12:11:07 +0700

    update --  change location of practice_7 to folder ph26105

--- ph26105/practice_7/Kysu.java
+++ ph26105/practice_7/Kysu.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package practice_7;
+package ph26105.practice_7;
 
 /**
  *
